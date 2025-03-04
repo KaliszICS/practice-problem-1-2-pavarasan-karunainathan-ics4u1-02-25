5b2a7146b400035cb78d320bddaaf13ebe8ac999
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-03-04 14:55:10 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,27 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+	static boolean validIndex(int[] arr, int index){
+		try{
+			arr[index] = arr[index]; // ????
+			return true;
+		}catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
 	}
 
-	
+	static int divide(int num1, int num2){
+		try{
+			return num1/num2;
+		}catch(ArithmeticException e){
+			return 0;
+		}
+	}
 
+	static int safeConvertStringtoInt(String str){
+		try{
+			return Integer.parseInt(str);
+		}catch(NumberFormatException e){
+			return 0;
+		}
+	}
+	public static void main(String args[]){}
 }
